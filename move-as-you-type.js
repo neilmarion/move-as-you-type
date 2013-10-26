739b4ff39339283c6079e3266122448a071872cb
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-10-26 12:19:53 +0800

    Some modifications and added an external css

--- move-as-you-type.js
+++ move-as-you-type.js
@@ -1,11 +1,22 @@
+var text = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog."
+
+function processText(t){
+  var processedText = ""
+  for(var x=0; x<t.length; x++){
+    processedText = processedText + '<div class="char">'+ t[x] +'</div>'
+  }
+  return processedText
+}
+
 $(document).ready(function(){
   winWidth = $(document).width()
   margin = (winWidth/2) - 20
   $('.content').css({"margin-left": margin.toString() + "px"})
+  $('.content').append(processText(text))
 })
 
 $(document).keypress(function(e){
   $('.content').animate({
     'marginLeft' : "-=29px"
-  }, 100)
+  }, 50)
 })
